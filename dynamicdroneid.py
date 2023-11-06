08987cc389e5430f572e077fe36835f5fea58acd
Kevin Racktoo <kevin@overheadintel.com>
2023-11-06 19:15:30 +0000

    Store droneid in variable upon heartbeat received

--- dynamicdroneid.py
+++ dynamicdroneid.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from pymavlink import mavutil
 import socket
 
+droneid = 0 # Initialize variable to store droneID that will be retrieved later
+
 # Function to retrieve local IP address as string
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,7 +18,8 @@ def wait_heartbeat(m):
     '''wait for heartbeat so we know target system IDs'''
     print("Waiting for APM heartbeat")
     msg = m.recv_match(type='HEARTBEAT', blocking=True)
-    print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_component))
+    droneid = m.target_system
+    print("Heartbeat from APM (system %u component %u)" % (droneid, m.target_component))
 
 # Initialize serial connection on UART2 to flight controller
 master = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600, source_system=255)
