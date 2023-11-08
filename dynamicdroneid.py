e28f9312f9a6dc82ea0ee5e48420f48d4d432876
Kevin Racktoo <kevin@overheadintel.com>
2023-11-08 17:32:45 +0000

    Cleaned up code a bit for demonstration, commented unused portions.

--- dynamicdroneid.py
+++ dynamicdroneid.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from pymavlink import mavutil
 import socket
 import os
+import time
 
 droneid = 0 # Initialize variable to store droneID that will be retrieved later
 dronecomponent = 0 # Intiialize variable to store droneComponent to 0
@@ -11,52 +12,71 @@ def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
-str = get_ip_address() # Retrieve local IP address
-str = str.split('.') # Subdivide into groups delimited by '.'
-newid = str[3] # Save last three digits as 'newid'
 
 # Function to wait for heartbeat packets and report back sysid
 def wait_heartbeat(m):
-    '''wait for heartbeat so we know target system IDs'''
-    print("Waiting for APM heartbeat")
+    '''Wait for heartbeat from connected drone and store droneID and componentID.'''
+    print("Waiting for heartbeat")
     msg = m.recv_match(type='HEARTBEAT', blocking=True)
     droneid = m.target_system # Store droneid
     dronecomponent = m.target_component # Store component
 
-# Grant read/write access to USB device.
-print("Making USB devices accesible...")
-os.system('sudo chmod a+rw /dev/ttyACM0')
+#################################### Start of program ########################################
+### Retrieve local IP address and process
+print("\n\n########################## IP RETRIEVAL ##############################")
+time.sleep(1)
+print("Obtaining local IP address...")
+str = get_ip_address()
+time.sleep(0.5)
+print("Splitting into groups delimited by '.'")
+str = str.split('.')
+time.sleep(0.5)
+newid = 66 #str[3]
+print(f'The last three digits of the local IP address are: {newid}\n\n')
+time.sleep(0.5)
 
-# Initialize USB connection to flight controller
+print("##################### CONNECT TO FLIGHT CONTROLLER #########################")
+time.sleep(1)
+### Grant read/write access to USB device.
+print("Making USB devices accessible...")
+os.system('sudo chmod a+rw /dev/ttyACM0')
+print("Success.")
+time.sleep(0.5)
+
+### Initialize USB connection to flight controller
 print("Connecting to the flight controller...")
 master = mavutil.mavlink_connection('/dev/ttyACM0', baud=57600, source_system=255)
 
-# Wait for heartbeat
+### Wait for heartbeat
 wait_heartbeat(master)
-print("Heartbeat received! The drone is connected.")
+print("Heartbeat message received; flight controller  is CONNECTED.\n\n")
+time.sleep(1)
 
 # TESTING better way to retrieve existing droneid
-master.mav.param_request_read_send(
-        master.target_system,
-        master.target_component,
-        b'SYSID_THISMAV',
-        -1
-)
+#master.mav.param_request_read_send(
+#        master.target_system,
+#        master.target_component,
+#        b'SYSID_THISMAV',
+#        -1
+#)
 
-while True:
-    try:
-        message = master.recv_match(type='SYSID_THISMAV', blocking=True)
-        if message is not None and message.param_id.decode('utf-8') == 'SYSID_THISMAV':
-            print(f"Parameter: {message.param_id}, value: {message.param_value}")
-            break
-    except Exception as e:
-        print(f"Error: {e}")
-        break
+#while True:
+#    try:
+#        message = master.recv_match(type='SYSID_THISMAV', blocking=True)
+#        if message is not None and message.param_id.decode('utf-8') == 'SYSID_THISMAV':
+#            print(f"Parameter: {message.param_id}, value: {message.param_value}")
+#            break
+#    except Exception as e:
+#        print(f"Error: {e}")
+#        break
 
 # Compare the drone's ID to the IP-based value
+print("######################## CONFIGURE SYSID ###############################")
 if master.target_system != newid: 
     # Set droneID to newid
-    print("Setting droneID of drone ", master.target_system)
+    print(f"Current sysID: {master.target_system}")
+    time.sleep(0.5)
+    print(f"Setting sysID to: {newid}")
     master.mav.param_set_send(
             master.target_system,
             master.target_component,
@@ -64,6 +84,7 @@ if master.target_system != newid:
             157,
             mavutil.mavlink.MAV_PARAM_TYPE_REAL32
     )
+    time.sleep(0.5)
     print("Writing parameters to EEPROM...")
     master.mav.command_long_send(
             droneid,
@@ -71,6 +92,7 @@ if master.target_system != newid:
             mavutil.mavlink.MAV_CMD_PREFLIGHT_STORAGE,
             1,0,0,0,0,0,0,0
     )
+    time.sleep(0.5)
     print("Rebooting drone...")
     master.mav.command_long_send(
             master.target_system,
@@ -78,6 +100,10 @@ if master.target_system != newid:
             mavutil.mavlink.MAV_CMD_PREFLIGHT_REBOOT_SHUTDOWN,
             0,1,0,0,0,0,0,0
     )
+    time.sleep(0.5)
+    print("Closing connection to flight controller...")
     master.close()
+    time.sleep(1)
+    print(f"SUCCESS: Drone ID has been set to {newid} and parameters have been saved.")
 else:
-    print("DroneID is already set. Shutting down.")
+    print("DroneID is already set. Shutting down...")
